06c340e4fa1242be6238e788335f818c24488266
JonahPeck <PeckJonahC@gmail.com>
2023-03-01 14:57:39 -0700

    finalized with Patch

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToyCard({ showForm, onRemoveListing }) {
 
   const { id, name, image, likes } = showForm;
+  const [toyLikes, setToyLikes] = useState(likes)
+
+  function handleLike() {
+    const newLikes = { likes: toyLikes + 1 }
+
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newLikes)
+    })
+      .then((response) => response.json())
+      .then(data => {
+        setToyLikes(data.likes);
+      })
+      .catch(error => console.error(error))
+  }
 
   function handleDeleteClick() {
     fetch(`http://localhost:3001/toys/${id}`, {
@@ -19,8 +37,8 @@ function ToyCard({ showForm, onRemoveListing }) {
         alt={image}
         className="toy-avatar"
       />
-      <p>{likes} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
+      <p>{toyLikes} Likes </p>
+      <button onClick={handleLike} className="like-btn">Like {"<3"}</button>
       <button onClick={handleDeleteClick} className="del-btn">Donate to GoodWill</button>
     </div>
   );
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({ showForm, onRemoveListing }) {
+function ToyContainer({ showForm, onRemoveListing, handleLike }) {
 
   const toyCards = showForm.map((toys) => (
-    <ToyCard key={toys.id} showForm={toys} onRemoveListing={onRemoveListing} />
+    <ToyCard key={toys.id} showForm={toys} onRemoveListing={onRemoveListing} handleLike={handleLike} />
   ))
   return (
     <div id="toy-collection">{toyCards}</div>
